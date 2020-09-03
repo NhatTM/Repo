b4c3252edfd422387a34c9ba9fe57fe86cd46373
NhatTM <usctz50@gmail.com>
2020-09-03 08:32:49 +0700

    Update btc-price.rs

--- btc-price.rs
+++ btc-price.rs
@@ -25,26 +25,5 @@ fn execute_impl(_: Input) -> Output {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use obi::get_schema;
-    use std::collections::*;
-
-    #[test]
-    fn test_get_schema() {
-        let mut schema = HashMap::new();
-        Input::add_definitions_recursively(&mut schema);
-        Output::add_definitions_recursively(&mut schema);
-        let input_schema = get_schema(String::from("Input"), &schema);
-        let output_schema = get_schema(String::from("Output"), &schema);
-        println!("{}/{}", input_schema, output_schema);
-        assert_eq!(
-            "{multiplier:u64}/{price:u64}",
-            format!("{}/{}", input_schema, output_schema),
-        );
-    }
-}
-
 prepare_entry_point!(prepare_impl);
 execute_entry_point!(execute_impl);
